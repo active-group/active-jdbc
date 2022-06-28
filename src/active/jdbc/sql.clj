2195e3b5ea28628c66131b11c5ce471359b6cdad
David Frese <david.frese@active-group.de>
2022-06-28 11:32:08 +0200

    Fix 'and' and 'or' to put parens around subexpressions. Add some more sql utils.

--- src/active/jdbc/sql.clj
+++ src/active/jdbc/sql.clj
@@ -1,11 +1,46 @@
 (ns active.jdbc.sql
   (:require [active.jdbc.query :as q])
-  (:refer-clojure :exclude [and or]))
+  (:refer-clojure :exclude [and or in list]))
+
+(let [open  ["("]
+      close [")"]]
+  (defn parens [q]
+    (q/concat0 open q close)))
 
 (let [_or_ ["OR"]]
   (defn or [q & qs]
-    (apply q/concat (interpose _or_ (cons q qs)))))
+    (if (empty? qs)
+      q
+      (apply q/concat (interpose _or_ (map parens (cons q qs)))))))
 
 (let [_and_ ["AND"]]
   (defn and [q & qs]
-    (apply q/concat (interpose _and_ (cons q qs)))))
+    (if (empty? qs)
+      q
+      (apply q/concat (interpose _and_ (map parens (cons q qs)))))))
+
+(let [_comma_ [","]]
+  (defn list [& things]
+    (apply q/concat (interpose _comma_ things))))
+
+(defn value [v]
+  ["?" v])
+
+(defn values [& vs]
+  (apply list (map value values)))
+
+(let [_in_ ["IN"]
+      _not_in_ ["NOT IN"]
+      with (fn [x e vs]
+             (q/concat e x (parens (apply values vs))))]
+  (defn in [e vs]
+    (with _in_ e values))
+
+  (defn not-in [e values]
+    (with _not_in_ e values)))
+
+(let [_WHERE_ ["WHERE"]]
+  (defn where [e]
+    (if (some? e)
+      (q/concat _WHERE_ e)
+      q/empty)))
