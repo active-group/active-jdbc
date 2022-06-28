2a9e762d223397774f36983e6bdc9dd60adfd646
David Frese <david.frese@active-group.de>
2022-06-28 11:32:18 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/active-jdbc "0.1.0"
+(defproject de.active-group/active-jdbc "0.2.0"
   :description "Functions to work with JDBC building upon next.jdbc."
   :url "http://github.com/active-group/active-jdbc"
   
@@ -6,11 +6,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.seancorfield/next.jdbc "1.2.780"]]
-
-  :codox {:language :clojure
-          :metadata {:doc/format :markdown}
-          :themes [:rdash]
-          :src-dir-uri "http://github.com/active-group/active-jdbc/blob/master/"
-          :src-linenum-anchor-prefix "L"}
-  )
+                 [com.github.seancorfield/next.jdbc "1.2.780"]])
